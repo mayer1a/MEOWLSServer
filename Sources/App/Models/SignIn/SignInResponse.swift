5c62be172220a23095ed76f11a90711bd83cc715
Artem V. Mayer <mayer1art@gmail.com>
2023-02-18 16:13:21 +0700

    add SignInResponse model

---
+++ Sources/App/Models/SignIn/SignInResponse.swift
@@ -0,0 +1,13 @@
+//
+//  SignInResponse.swift
+//  
+//
+//  Created by Artem Mayer on 18.02.2023.
+//
+
+import Vapor
+
+struct SignInResponse: Content {
+    var result: Int
+    var user: User
+}
