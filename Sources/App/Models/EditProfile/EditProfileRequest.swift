c4509cae9b00fc8b862b9ebb6cd8ea2fb199ba38
Artem V. Mayer <mayer1art@gmail.com>
2023-02-18 18:20:19 +0700

    add EditProfileRequest model

---
+++ Sources/App/Models/EditProfile/EditProfileRequest.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileRequest.swift
+//  
+//
+//  Created by Artem Mayer on 18.02.2023.
+//
+
+import Vapor
+
+struct EditProfileRequest: Content {
+    var user_id: Int
+    var username: String
+    var password: String
+    var email: String
+    var gender: Gender
+    var credit_card: String
+    var bio: String
+}
