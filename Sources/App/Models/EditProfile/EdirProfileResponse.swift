277261d2ead8e1cb8ff607817f46ac4a1c38a9cd
Artem V. Mayer <mayer1art@gmail.com>
2023-02-18 18:21:52 +0700

    add EdirProfileResponse model

---
+++ Sources/App/Models/EditProfile/EdirProfileResponse.swift
@@ -0,0 +1,13 @@
+//
+//  EdirProfileResponse.swift
+//  
+//
+//  Created by Artem Mayer on 18.02.2023.
+//
+
+import Vapor
+
+struct EdirProfileResponse: Content {
+    var result: Int
+    var error_message: String?
+}
