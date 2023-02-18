307faa40dc9c4a02479bd6c4b6cf535984909a78
Artem V. Mayer <mayer1art@gmail.com>
2023-02-18 18:43:04 +0700

    add GetCatalogController get catalog HTTP request processing class

---
+++ Sources/App/Controllers/GetCatalogController.swift
@@ -0,0 +1,41 @@
+//
+//  GetCatalogController.swift
+//  
+//
+//  Created by Artem Mayer on 18.02.2023.
+//
+
+import Vapor
+
+class GetCatalogController {
+
+    // MARK: - Functions
+
+    func get(_ req: Request) throws -> EventLoopFuture<GetCatalogResponse> {
+        guard
+            let body = try? req.content.decode(GetCatalogRequest.self)
+        else {
+            throw Abort(.badRequest)
+        }
+
+        print(body)
+
+        let products = [
+            ProductMainInfo(
+                product_id: 123,
+                product_name: "Ноутбук",
+                product_price: 45600),
+            ProductMainInfo(
+                product_id: 456,
+                product_name: "Компьютерная мышь",
+                product_price: 1000)
+        ]
+
+        let response = GetCatalogResponse(
+            result: 1,
+            page_number: 1,
+            products: products)
+
+        return req.eventLoop.future(response)
+    }
+}
