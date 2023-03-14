d93388be61adfbf0b6e4449f6701ff7231345eb6
Artem V. Mayer <mayer1art@gmail.com>
2023-03-14 11:56:41 +0700

    add new properties to SignInRequest

--- Sources/App/Models/SignIn/SignInRequest.swift
+++ Sources/App/Models/SignIn/SignInRequest.swift
@@ -1,6 +1,6 @@
 //
 //  SignInRequest.swift
-//  
+//
 //
 //  Created by Artem Mayer on 18.02.2023.
 //
@@ -10,6 +10,6 @@ import Vapor
 // MARK: - SignInRequest
 
 struct SignInRequest: Content {
-    var username: String
+    var email: String
     var password: String
 }
