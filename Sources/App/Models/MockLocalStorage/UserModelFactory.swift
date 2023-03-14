377e12aec1e4e4703a4166a6615e35d8b285face
Artem V. Mayer <mayer1art@gmail.com>
2023-03-14 11:53:48 +0700

    add UserModelFactory

--- Sources/App/Models/MockLocalStorage/UserModelFactory.swift
+++ Sources/App/Models/MockLocalStorage/UserModelFactory.swift
@@ -1,8 +1,37 @@
 //
-//  File.swift
-//  
+//  UserModelFactory.swift
 //
-//  Created by Artem Mayer on 14.03.2023.
+//
+//  Created by Artem Mayer on 13.03.2023.
 //
 
 import Foundation
+
+struct UserModelFactory {
+
+    // MARK: - Functions
+
+    func construct(from user: SignUpRequest, with id: Int) -> User {
+        User(
+            user_id: id,
+            name: user.name,
+            lastname: user.lastname,
+            username: user.username,
+            email: user.email,
+            credit_card: user.credit_card,
+            gender: user.gender,
+            bio: user.bio)
+    }
+
+    func construct(from user: RawUpdateUserModel) -> User {
+        User(
+            user_id: user.user_id,
+            name: user.name,
+            lastname: user.lastname,
+            username: user.username,
+            email: user.email,
+            credit_card: user.credit_card,
+            gender: user.gender,
+            bio: user.bio)
+    }
+}
