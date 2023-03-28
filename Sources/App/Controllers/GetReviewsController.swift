3bd29e5958c155b514f04ac8dfdea814a426a948
Artem V. Mayer <mayer1art@gmail.com>
2023-03-28 15:24:35 +0700

    add MockProductsReview, fix GetReviewsResponse and Review models, add logic to GetReviewsController

--- Sources/App/Controllers/GetReviewsController.swift
+++ Sources/App/Controllers/GetReviewsController.swift
@@ -22,21 +22,49 @@ class GetReviewsController {
 
         print(body)
 
-        let reviews = [
-            Review(
-                review_id: 111,
-                product_id: 456,
-                user_id: 123,
-                description: "Хорошая мышь"),
-            Review(
-                review_id: 112,
-                product_id: 456,
-                description: "Стоит своих денег!")
-        ]
+        if body.page_number < 0 {
+            let response = GetReviewsResponse(result: 0, error_message: "Отзывы не найдены. Неверный номер страницы!")
+            return req.eventLoop.future(response)
+        }
+
+        let elementsInCollection = 20
+        let relatedReviews = MockProductsReviews.shared.reviews.compactMap { review -> Review? in
+            guard review.product_id == body.product_id else { return nil }
+            return review
+        }
+
+        let reviewsLastIndex = relatedReviews.count - 1
+        var range: ClosedRange<Int>
+
+        if body.page_number == 1 {
+            let startIndex = 0
+            var endIndex = elementsInCollection - 1
+            endIndex = reviewsLastIndex < endIndex ? reviewsLastIndex : endIndex
+            range = startIndex...endIndex
+        } else if body.page_number == 0 {
+            let startIndex = 0
+            var endIndex = 2
+            endIndex = reviewsLastIndex < endIndex ? reviewsLastIndex : endIndex
+            range = startIndex...endIndex
+        } else {
+            let startIndex = body.page_number * 20
+
+            guard startIndex >= reviewsLastIndex else {
+                throw Abort(.noContent)
+            }
+
+            var endIndex = startIndex + elementsInCollection - 1
+            endIndex = reviewsLastIndex < endIndex ? reviewsLastIndex : endIndex
+            range = startIndex...endIndex
+        }
+
+        let reviews = Array(relatedReviews[range])
+        let nextPage = range.last ?? .max < reviewsLastIndex ? body.page_number + 1 : nil
 
         let response = GetReviewsResponse(
             result: 1,
-            reviews: reviews)
+            reviews: reviews,
+            next_page: nextPage)
 
         return req.eventLoop.future(response)
     }
