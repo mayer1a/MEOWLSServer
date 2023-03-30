3dca3d9c750b277014fb73bcc412a05e76869fe5
Artem V. Mayer <mayer1art@gmail.com>
2023-03-30 13:10:56 +0700

    3 controllers associated with the basket are merged into one, methods for changing  in the basket

--- Sources/App/Controllers/BasketController.swift
+++ Sources/App/Controllers/BasketController.swift
@@ -1,5 +1,5 @@
 //
-//  AddProductController.swift
+//  BasketController.swift
 //  
 //
 //  Created by Artem Mayer on 01.03.2023.
@@ -9,14 +9,12 @@ import Vapor
 
 // MARK: - AddProductController
 
-class AddProductController {
+final class BasketController {
 
     // MARK: - Functions
 
     func addProduct(_ req: Request) throws -> EventLoopFuture<AddProductResponse> {
-        guard
-            let model = try? req.content.decode(AddProductRequest.self)
-        else {
+        guard let model = try? req.content.decode(AddProductRequest.self) else {
             throw Abort(.badRequest)
         }
 
@@ -31,5 +29,85 @@ class AddProductController {
 
         let response = AddProductResponse(result: 1)
         return req.eventLoop.future(response)
+    }
+
+    func removeProduct(_ req: Request) throws -> EventLoopFuture<RemoveProductResponse> {
+        guard let model = try? req.content.decode(RemoveProductRequest.self) else {
+            throw Abort(.badRequest)
+        }
+
+        print(model)
+        
+        let isRemoved = MockBasket.shared.removeProduct(userId: model.user_id, productId: model.product_id)
+
+        guard isRemoved else {
+            let response = RemoveProductResponse(result: 0, error_message: "Товар отсутствует в корзине!")
+            return req.eventLoop.future(response)
+        }
+
+        let response = RemoveProductResponse(result: 1)
+
+        return req.eventLoop.future(response)
+    }
+
+    func editProductQuantity(_ req: Request) throws -> EventLoopFuture<EditProductResponse> {
+        guard let model = try? req.content.decode(EditProductRequest.self) else {
+            throw Abort(.badRequest)
+        }
+
+        print(model)
+
+        let isEdited = MockBasket.shared.editProductQuantity(
+            userId: model.user_id,
+            productId: model.product_id,
+            quantity: model.new_quantity)
+
+        guard isEdited else {
+            let response = EditProductResponse(result: 0, error_message: "Товар отсутствует в корзине!")
+            return req.eventLoop.future(response)
+        }
+
+        let response = EditProductResponse(result: 1)
+
+        return req.eventLoop.future(response)
+    }
+
+    func pay(_ req: Request) throws -> EventLoopFuture<PayBasketResponse> {
+        guard let model = try? req.content.decode(PayBasketRequest.self) else {
+            throw Abort(.badRequest)
+        }
+
+        print(model)
+
+        guard let userBasket = MockBasket.shared.getBasket(for: model.user_id) else {
+            let response = PayBasketResponse(result: 0, error_message: "Корзина не найдена!")
+            return req.eventLoop.future(response)
+        }
+
+        let basketAmount = getBasketAmount(userBasket)
+        let isBasketPaid = emulatedBankRequest(purchaseAmount: basketAmount)
+
+        guard isBasketPaid else {
+            let response = PayBasketResponse(result: 0, error_message: "Отказ со стороны банка!")
+            return req.eventLoop.future(response)
+        }
+
+        MockBasket.shared.clearAfterPurchase(for: model.user_id)
+
+        let response = PayBasketResponse(result: 1, user_message: "Покупки успешно оплачены. Чек выслан на почту.")
+
+        return req.eventLoop.future(response)
+    }
+
+    // MARK: - Private functions
+
+    private func emulatedBankRequest(purchaseAmount: Int) -> Bool {
+        purchaseAmount <= 47500
+    }
+
+    private func getBasketAmount(_ basket: UserBasket) -> Int {
+        basket.getBasket().reduce(0) { partialResult, element in
+            partialResult + element.key.product_price * element.value
+        }
     }
 }
