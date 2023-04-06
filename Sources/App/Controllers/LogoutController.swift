3227c1176f4b6527f1d75db49edb29a5bdefe3b6
Artem V. Mayer <mayer1art@gmail.com>
2023-04-06 20:52:54 +0700

    add to LogoutController localStorage property and business logic

--- Sources/App/Controllers/LogoutController.swift
+++ Sources/App/Controllers/LogoutController.swift
@@ -1,6 +1,6 @@
 //
 //  LogoutController.swift
-//  
+//
 //
 //  Created by Artem Mayer on 18.02.2023.
 //
@@ -11,18 +11,35 @@ import Vapor
 
 class LogoutController {
 
+    // MARK: - Properties
+
+    let localStorage: LocalStorage
+
+    // MARK: - Constructions
+
+    init(localStorage: LocalStorage) {
+        self.localStorage = localStorage
+    }
+
     // MARK: - Functions
 
     func logout(_ req: Request) throws -> EventLoopFuture<LogoutResponse> {
         guard
-            let body = try? req.content.decode(LogoutRequest.self)
+            let model = try? req.content.decode(LogoutRequest.self)
         else {
             throw Abort(.badRequest)
         }
 
-        print(body)
+        print(model)
 
-        let response = LogoutResponse(result: 1)
+        let isUserExists = localStorage.read(by: model.user_id) != nil
+        var response: LogoutResponse
+
+        if isUserExists {
+            response = LogoutResponse(result: 1)
+        } else {
+            response = LogoutResponse(result: 0)
+        }
 
         return req.eventLoop.future(response)
     }
