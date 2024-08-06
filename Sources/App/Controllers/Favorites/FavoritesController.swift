a49fb76788055f00758390ba8815e522cc1ef1b6
Artem Mayer <mayer1art@gmail.com>
2024-08-06 21:14:27 +0700

    T-2 Add SalesController, SalesRepository
    Add FavoritesController, FavoritesRepository

---
+++ Sources/App/Controllers/Favorites/FavoritesController.swift
@@ -0,0 +1,54 @@
+//
+//  FavoritesController.swift
+//
+//
+//  Created by Artem Mayer on 02.08.2024.
+//
+
+import Vapor
+import Fluent
+
+struct FavoritesController: RouteCollection {
+
+    let favoritesRepository: FavoritesRepositoryProtocol
+
+    @Sendable func boot(routes: RoutesBuilder) throws {
+
+        let favorites = routes.grouped("api", "v1", "favorites")
+
+        let tokenAuthGroup = favorites.grouped(Token.authenticator(), User.guardMiddleware())
+        tokenAuthGroup.get("", use: get)
+        tokenAuthGroup.post("star", use: starProduct)
+        tokenAuthGroup.post("unstar", use: unstarProduct)
+    }
+
+    @Sendable private func get(_ request: Request) async throws -> FavoritesDTO {
+
+        guard let user = request.auth.get(User.self) else { throw Abort(.unauthorized) }
+
+        return try await favoritesRepository.get(for: user)
+    }
+
+    @Sendable private func starProduct(_ request: Request) async throws -> DummyResponse {
+
+        guard let user = request.auth.get(User.self) else { throw Abort(.unauthorized) }
+
+        let productsIDs = try request.content.decode([UUID].self)
+
+        try await favoritesRepository.update(productsIDs: productsIDs, for: user)
+
+        return DummyResponse()
+    }
+
+    @Sendable private func unstarProduct(_ request: Request) async throws -> DummyResponse {
+
+        guard let user = request.auth.get(User.self) else { throw Abort(.unauthorized) }
+
+        let productsIDs = try request.content.decode([UUID].self)
+
+        try await favoritesRepository.delete(productsIDs: productsIDs, for: user)
+
+        return DummyResponse()
+    }
+
+}
--- Sources/App/Routes/RegisterRoutes.swift
+++ Sources/App/Routes/RegisterRoutes.swift
@@ -16,10 +16,14 @@ struct RegisterRoutes {
         app.get("health-check") { _ async throws in DummyResponse() }
 
         let tokenRepository = TokenRepository(database: app.db)
+        let favoritesRepository = FavoritesRepository(database: app.db)
         let userRepository = UserRepository(database: app.db, with: favoritesRepository, tokenRepository)
         let bannersRepository = BannersRepository(database: app.db, cache: app.caches)
+        let salesRepository = SalesRepository(database: app.db, cache: app.caches)
         try app.register(collection: UserController(with: userRepository, tokenRepository))
         try app.register(collection: BannersController(bannersRepository: bannersRepository))
+        try app.register(collection: FavoritesController(favoritesRepository: favoritesRepository))
+        try app.register(collection: SalesController(salesRepository: salesRepository))
 
         app.routes.print()
     }
