3b208c9345b695985c500c1714a550194cfbbb1c
Artem Mayer <mayer1art@gmail.com>
2024-08-06 20:10:08 +0700

    T-2 Remove all legacy

--- Sources/App/Routes/RegisterRoutes.swift
+++ Sources/App/Routes/RegisterRoutes.swift
@@ -8,8 +8,6 @@
 import Fluent
 import Vapor
 
-// MARK: - Functions
-
 struct RegisterRoutes {
 
     static func register(for app: Application) throws {
@@ -17,8 +15,6 @@ struct RegisterRoutes {
         // For render.com
         app.get("health-check") { _ async throws in DummyResponse() }
 
-        try app.register(collection: CatalogController())
-        try app.register(collection: UserController())
 
         app.routes.print()
     }
