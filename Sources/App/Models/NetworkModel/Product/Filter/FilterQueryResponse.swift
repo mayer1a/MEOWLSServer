27a148703df913165f0741592f02565db37a73a3
Artem Mayer <mayer1art@gmail.com>
2024-09-08 04:56:16 +0700

    T-14 Fix encoded url error
    Add decoder init, remove replacingPercentEncoding from ProductsController

--- Sources/App/Models/NetworkModel/Product/Filter/FilterQueryResponse.swift
+++ Sources/App/Models/NetworkModel/Product/Filter/FilterQueryResponse.swift
@@ -8,6 +8,23 @@
 import Vapor
 
 struct FilterQueryRequest: Content {
+
     let filters: [String: [String]]?
     let sort: String?
+
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.sort = try container.decodeIfPresent(String.self, forKey: .sort)
+
+        var filters: [String: [String]] = [:]
+        if let filtersKey = container.allKeys.first(where: { $0.stringValue == "filters" }) {
+            let values = try container.decode([String: [String]].self, forKey: filtersKey)
+            values.forEach { key, values in
+                filters[key] = values.compactMap({ $0.removingPercentEncoding })
+            }
+        }
+
+        self.filters = filters
+    }
+
 }
